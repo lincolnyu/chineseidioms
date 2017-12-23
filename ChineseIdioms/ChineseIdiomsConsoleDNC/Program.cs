ba35644c1cb4a44b8f05366f2952cc164a1ad2bb
Lincoln Yu <quanben@msn.com>
2017-12-24 01:42:10 +1100

    Implement a dotnetcore console version.

--- ChineseIdioms/ChineseIdiomsConsoleDNC/Program.cs
+++ ChineseIdioms/ChineseIdiomsConsoleDNC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ChineseIdioms.Io;
 using ChineseIdioms.Linkage;
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -20,7 +21,7 @@ namespace ChineseIdiomsConsole
             _idiomsLookup.Load(idioms);
         }
 
-        static void NextWithFirst(char first, StreamWriter sw)
+        static void NextWithFirst(char first, TextWriter tw)
         {
             var sols = _idiomsLookup.IdiomsAsFirstChar(first);
             if (sols != null)
@@ -28,17 +29,17 @@ namespace ChineseIdiomsConsole
                 var num = 1;
                 foreach (var sol in sols)
                 {
-                    sw.WriteLine($"解{num}: {sol}");
+                    tw.WriteLine($"解{num}: {sol}");
                     num++;
                 }
             }
             else
             {
-                sw.WriteLine($"“{first}”起首无解");
+                tw.WriteLine($"“{first}”起首无解");
             }
         }
 
-        static void FindWidest(char first, StreamWriter sw, HashSet<string> excluded = null)
+        static void FindWidest(char first, TextWriter tw, HashSet<string> excluded = null)
         {
             var sol = _idiomsLookup.GetWidestPath(first, 5, excluded);
             if (sol != null)
@@ -48,48 +49,134 @@ namespace ChineseIdiomsConsole
                 {
                     if (notFirst)
                     {
-                        sw.Write("->");
+                        tw.Write("->");
                     }
                     else
                     {
                         notFirst = true;
                     }
-                    sw.Write($"{idiom}");
+                    tw.Write($"{idiom}");
                 }
-                sw.WriteLine();
+                tw.WriteLine();
             }
             else
             {
-                sw.WriteLine($"“{first}”起首无解");
+                tw.WriteLine($"“{first}”起首无解");
             }
         }
 
-        static void Join(char start, char end, StreamWriter sw)
+        static void Join(char start, char end, TextWriter tw)
         {
             var sols = _idiomsLookup.JoinEnds(start, end);
             var num = 1;
             foreach (var sol in sols)
             {
-                sw.Write($"解{num}: ");
+                tw.Write($"解{num}: ");
                 var notFirst = false;
                 foreach (var idiom in sol)
                 {
                     if (notFirst)
                     {
-                        sw.Write("->");
+                        tw.Write("->");
                     }
                     else
                     {
                         notFirst = true;
                     }
-                    sw.Write(idiom);
+                    tw.Write(idiom);
                 }
-                sw.WriteLine();
+                tw.WriteLine();
                 num++;
             }
             if (num == 1)
             {
-                sw.WriteLine($"连接“{start}”和“{end}”无解");
+                tw.WriteLine($"连接“{start}”和“{end}”无解");
+            }
+        }
+
+        abstract class Request
+        {
+            public abstract void Action(TextWriter tw);
+        }
+
+        class JoinRequest : Request
+        {
+            public char FirstChar;
+            public char LastChar;
+
+            public override void Action(TextWriter tw)
+            {
+                Join(FirstChar, LastChar, tw);
+            }
+        }
+
+        class InitialRequest: Request
+        {
+            public char FirstChar;
+
+            public override void Action(TextWriter tw)
+            {
+                NextWithFirst(FirstChar, tw);
+            }
+        }
+
+        class WidestPathRequest: Request
+        {
+            public char FirstChar;
+            public HashSet<string> ExcludeSet;
+
+            public override void Action(TextWriter tw)
+            {
+                FindWidest(FirstChar, tw, ExcludeSet);
+            }
+        }
+        
+        static Request ParseInput(TextReader sr)
+        {
+            var cmd = sr.ReadLine();
+            if (string.IsNullOrWhiteSpace(cmd)) return null;
+            switch (cmd)
+            {
+                case "first":
+                {
+                    var line = sr.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line)) return null;
+                    return new InitialRequest
+                    {
+                        FirstChar = line.TrimStart()[0]
+                    };
+                }
+                case "join":
+                {
+                    var line1 = sr.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line1)) return null;
+                    var line2 = sr.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line2)) return null;
+                    return new JoinRequest
+                    {
+                        FirstChar = line1.TrimStart()[0],
+                        LastChar = line2.TrimStart()[0]
+                    };
+                }
+                case "widest":
+                {
+                    var first = sr.ReadLine();
+                    if (string.IsNullOrWhiteSpace(first)) return null;
+                    var excludeSet = new HashSet<string>();
+                    while (true)
+                    {
+                        var line = sr.ReadLine().Trim();
+                        if (string.IsNullOrEmpty(line)) break;
+                        excludeSet.Add(line.Trim());
+                    }
+                    return new WidestPathRequest
+                    {
+                        FirstChar = first.TrimStart()[0],
+                        ExcludeSet = excludeSet
+                    };
+                }
+                default:
+                    return null;
             }
         }
 
@@ -98,34 +185,17 @@ namespace ChineseIdiomsConsole
             try
             {
                 InitIdiomRepo();
-                using (var sw = new StreamWriter(args[0]))
+                var inputFilePath = args[0];
+                var outputFilePath = args[1];
+                using (var inputFile = new FileStream(inputFilePath, FileMode.Open))
+                using (var sr = new StreamReader(inputFile, Encoding.UTF8))
+                using (var outputFile = new FileStream(outputFilePath, FileMode.Create))
+                using (var sw = new StreamWriter(outputFile, Encoding.UTF8))
                 {
-                    var action = args[1];
-                    if (action == "join")
-                    {
-                        Join(args[2][0], args[3][0], sw);
-                    }
-                    else if (action == "first")
-                    {
-                        NextWithFirst(args[2][0], sw);
-                    }
-                    else if (action == "widest")
-                    {
-                        HashSet<string> excludeSet = null;
-                        if (args.Length > 3)
-                        {
-                            var excludes = args[3].Split(',');
-                            excludeSet = new HashSet<string>();
-                            foreach (var e in excludes)
-                            {
-                                excludeSet.Add(e);
-                            }
-                        }
-                        FindWidest(args[2][0], sw, excludeSet);
-                    }
+                    var req = ParseInput(sr);
+                    req?.Action(sw);
                 }
-                Process.Start(args[0]);
-                // TODO ...
+                Process.Start("notepad.exe", outputFilePath);
             }
             catch (Exception e)
             {
